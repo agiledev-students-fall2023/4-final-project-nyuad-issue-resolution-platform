52ae8ff8ff0e51946501fa523d259d9ce5f2e0d9
hasiburratul <hasibur.ratul@gmail.com>
2023-10-14 01:40:13 -0400

    Implemting Student Dasboard from user story #14

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
+import "./App.css";
+import StudentDashboard from "./StudentDashboard";
+import IssueDetails from "./IssueDetails";
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <main className="App-main">
+          <Routes>
+            <Route path="/" element={<StudentDashboard />} />
+            <Route path="/issue/:index" element={<IssueDetails />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
