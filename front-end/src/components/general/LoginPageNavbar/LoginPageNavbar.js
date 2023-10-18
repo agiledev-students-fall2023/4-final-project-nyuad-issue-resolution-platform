5cba3a220ab2dea1d20f8356b1a8763700700766
swostikpati <swostikpati@gmail.com>
2023-10-18 11:06:52 -0400

    implemented login page navbar header

--- front-end/src/components/general/LoginPageNavbar/LoginPageNavbar.js
+++ front-end/src/components/general/LoginPageNavbar/LoginPageNavbar.js
@@ -3,13 +3,13 @@ import logo from "../../../assets/images/nyuad-logo.png";
 
 const LoginPageNavbar = () => {
     return (
-        <header className="header">
+        <header className="header-login">
             <a href='/' className='navbar-nyuad-text'><p>NYU ABU DHABI <span className='seperator'> | </span> ISSUE RESOLUTION</p></a>
-            <a href="https://nyuad.nyu.edu/en/" id="nyuad-logo"><img src={logo} alt="logo" /></a>
+            <a href="https://nyuad.nyu.edu/en/" id="nyuad-logo" target="_blank" rel="noopener noreferrer"><img src={logo} alt="logo" /></a>
             <nav>
                 <ul className="nav-links">
-                    <li><a href="https://students.nyuad.nyu.edu/">STUDENT PORTAL</a></li>
-                    <li><a href="https://intranet.nyuad.nyu.edu/">INTRANET</a></li>
+                    <li><a href="https://students.nyuad.nyu.edu/" target="_blank" rel="noopener noreferrer">STUDENT PORTAL</a></li>
+                    <li><a href="https://intranet.nyuad.nyu.edu/" target="_blank" rel="noopener noreferrer">INTRANET</a></li>
                 </ul>
             </nav>
         </header>
