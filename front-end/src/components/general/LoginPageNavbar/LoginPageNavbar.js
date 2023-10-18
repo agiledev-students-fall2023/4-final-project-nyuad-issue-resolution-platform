80d277076fc0f742cb4145308332d77d6038249a
swostikpati <swostikpati@gmail.com>
2023-10-18 02:40:21 -0400

    fixed small bugs and linted

--- front-end/src/components/general/LoginPageNavbar/LoginPageNavbar.js
+++ front-end/src/components/general/LoginPageNavbar/LoginPageNavbar.js
@@ -1,22 +1,19 @@
-/* eslint-disable */
-import React from 'react';
 import './LoginPageNavbar.css';
 import logo from "../../../assets/images/nyuad-logo.png";
 
-
 const LoginPageNavbar = () => {
     return (
-        <header class="header" id="heading1">
-            <a href='/'><p>NYU ABU DHABI <span className='seperator'> | </span> ISSUE RESOLUTION</p></a>
-            <a href="https://nyuad.nyu.edu/en/" id="logo"><img src={logo} alt="logo" /></a>
+        <header className="header">
+            <a href='/' className='navbar-nyuad-text'><p>NYU ABU DHABI <span className='seperator'> | </span> ISSUE RESOLUTION</p></a>
+            <a href="https://nyuad.nyu.edu/en/" id="nyuad-logo"><img src={logo} alt="logo" /></a>
             <nav>
-                <ul class="nav_links">
+                <ul className="nav-links">
                     <li><a href="https://students.nyuad.nyu.edu/">STUDENT PORTAL</a></li>
                     <li><a href="https://intranet.nyuad.nyu.edu/">INTRANET</a></li>
                 </ul>
             </nav>
         </header>
     );
-}
+};
 
 export default LoginPageNavbar;
