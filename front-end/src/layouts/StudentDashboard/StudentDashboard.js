34f03109775153f42c68d27f7cd886f2a4dd3b93
basil-ahmed <bq2024@nyu.edu>
2023-10-23 11:01:48 -0400

    Changed mobile overlay

--- front-end/src/layouts/StudentDashboard/StudentDashboard.js
+++ front-end/src/layouts/StudentDashboard/StudentDashboard.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./StudentDashboard.css";
 import StudentNavbar from "../../components/student/StudentNavbar/StudentNavbar";
 import StudentViewFilter from "../../components/student/StudentViewFilter/StudentViewFilter";
@@ -157,13 +157,10 @@ const StudentDashboard = () => {
       if (windowWidth <= 768) {
         return (
           <tr key={index}>
-            <td className="title-cell-mobile" onClick={() => {
-            setIsIssueOverlayOpen(true);
-            setRequest(request.index);
-        }}>
-              {/* <Link to={`/issue/${request.index}`} className="issue-link"> */}
+            <td className="title-cell-mobile">
+              <Link to={`/issue/${request.index}`} className="issue-link">
                 {request.title}
-              {/* </Link> */}
+              </Link>
             </td>
             <td>
               <span className={`status-box ${getStatusClass(request.currentStatus)}`}>
