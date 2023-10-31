1c10734d19e8dcad512add635c03543ef90a6dd5
nadamels <nme3923@nyu.edu>
2023-10-31 11:47:51 -0400

    API connected

--- front-end/src/components/student/CreateRequest.js
+++ front-end/src/components/student/CreateRequest.js
@@ -5,26 +5,46 @@ export function CreateRequest({ isVisible, onClose }) {
     const [issueTitle, setIssueTitle] = useState('');
     const [description, setDescription] = useState('');
     const [department, setDepartment] = useState('department1');
-    const [setSelectedFile] = useState(null);
-
+    const [selectedFile, setSelectedFile] = useState(null);
     const handleIssueTitleChange = (event) => {
         setIssueTitle(event.target.value);
     };
-
     const handleDescriptionChange = (event) => {
         setDescription(event.target.value);
     };
-
     const handleDepartmentChange = (event) => {
         setDepartment(event.target.value);
     };
-
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('issueTitle', issueTitle);
+        formData.append('description', description);
+        formData.append('department', department);
+        if (selectedFile) {
+            formData.append('file', selectedFile);
+        }
+        const apiUrl = "https://hasiburratul.github.io/mock-api/MOCK_DATA.json";
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                onClose(); // clse
+            } else {
+                console.error('Error during form submission:', response.status, response.statusText);
+            }
+        } catch (error) {
+            console.error('Error during form submission:', error);
+        }
+    };
 
     return (
-        <div id="createRequestComponent" className={`request-container ${isVisible ? 'visible' : ''}`}>
+        <div id="createRequestComponent" className={`create-request-component-student ${isVisible ? 'visible' : ''}`}>
             <div className="top-rectangle"></div>
             <div className="line"></div>
 
@@ -75,11 +95,11 @@ export function CreateRequest({ isVisible, onClose }) {
 
             <div className="input-group file-group">
                 <div>
-                    <label className='file-label'>Attach File(s)</label>
+                    <label htmlFor="fileInput" className='file-label'>Attach File(s)</label>
                 </div>
                 <input className='file-select' type="file" id="fileInput" onChange={handleFileChange} />
             </div>
-            <button type="submit" className='close-button' onClick={onClose}>Post Request</button>
+            <button type="submit" className='close-button' onClick={handleFormSubmit}>Post Request</button>
             <button className='x-button' onClick={onClose}>x</button>
         </div>
     );
