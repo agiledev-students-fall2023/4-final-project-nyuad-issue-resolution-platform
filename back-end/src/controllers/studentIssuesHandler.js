4cbd1b856ef0bcf467fd1b08c7d3f485e07c633d
swostikpati <swostikpati@gmail.com>
2023-11-04 19:47:27 -0400

    filtered mock student issue details in backend

--- back-end/src/controllers/studentIssuesHandler.js
+++ back-end/src/controllers/studentIssuesHandler.js
@@ -3,17 +3,21 @@ import axios from "axios";
 export async function issueRetrievalHandler(req, res) {
   const { paramName } = req.params;
 
-  if (paramName === "all") {
-    try {
-      // Assuming the data you want is at the response.data property
-      const response = await axios.get(
-        `${process.env.BACKEND_URL}/json/contextual_mockapi.json` // will be replaced with db call
-      );
-      res.json(response.data);
-    } catch (error) {
-      // Log the error and send an appropriate response
-      console.error("Error retrieving issues:", error.message);
-      res.status(500);
-    }
+  try {
+    // Assuming the data you want is at the response.data property
+    const response = await axios.get(
+      `${process.env.BACKEND_URL}/json/contextual_mockapi.json` // will be replaced with db call
+    );
+
+    // Assuming response.data is an array of items and each item has a studentNetId field
+    const filteredData = response.data.filter(
+      (item) => item.studentNetID[0] === paramName
+    );
+
+    res.json(filteredData); // Send only the data that matches the studentNetId
+  } catch (error) {
+    // Log the error and send an appropriate response
+    console.error("Error retrieving data:", error.message);
+    res.status(500).send("An error occurred while retrieving the data.");
   }
 }
