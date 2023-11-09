1b02886a0c2323a20d6011a22a88614586f286b8
nadamels <nme3923@nyu.edu>
2023-11-09 11:46:48 -0500

    addressed issues from previous commit

--- back-end/src/controllers/createIssueHandler.js
+++ back-end/src/controllers/createIssueHandler.js
@@ -1,23 +1,41 @@
 import { publicpath } from "../../app.js";
 import { promises as fs } from 'fs';
 
-export async function createIssueHandler(req, res){
+export async function createIssueHandler(req, res) {
     try {
-    const requestData = req.body.formData;
-    const filePath = publicpath + "/json/mockapi.json";
-    const fileContent = await fs.readFile(filePath, 'utf8');
-    const jsonData = JSON.parse(fileContent);
-
-    jsonData.push(formData);
-
-    await fs.writeFile(filePath, JSON.stringify(jsonData, null, '\t'));
-
-
-    console.log('File written successfully.');
-    res.status(200).json({ message: 'Data written to the file successfully' });
+      const {
+        studentNetID: ID,
+        studentName: name,
+        formData,
+        currentPriority,
+        dateCreated: date,
+        currentStatus,
+      } = req.body;
+  
+      const filePath = publicpath + '/json/mockapi.json';
+      const lastIndex = jsonData.length > 0 ? jsonData[jsonData.length - 1].index : 0;
+  
+      // read the existing JSON file
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      const jsonData = JSON.parse(fileContent);
+  
+      jsonData.push({
+        index: lastIndex + 1,
+        ID,
+        name,
+        formData,
+        currentPriority,
+        date,
+        currentStatus,
+      });
+  
+      // write the updated JSON back to the file
+      await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+  
+      console.log('Data written to the file successfully.');
+      res.status(200).json({ message: 'Data written to the file successfully' });
     } catch (error) {
-        console.error('Error while writing data to the file:', error);
-        res.status(500).json({ error: 'An error occurred while writing data to the file' });
+      console.error('Error while writing data to the file:', error);
+      res.status(500).json({ error: 'An error occurred while writing data to the file' });
     }
-
-}
+  }
