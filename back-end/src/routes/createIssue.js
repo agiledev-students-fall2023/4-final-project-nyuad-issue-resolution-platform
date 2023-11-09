409dfabd3ead3ea1d8db1e763398804ea289e60f
nadamels <nme3923@nyu.edu>
2023-11-09 08:42:57 -0500

    api endpoint

--- back-end/src/routes/createIssue.js
+++ back-end/src/routes/createIssue.js
@@ -1,9 +1,6 @@
 import { createIssueHandler } from '../controllers/createIssueHandler';
 import { express } from 'express';
 
-
-const express = require('express')
-
 const router = express.Router();
 
 router.post("/:paraName", createIssueHandler) //controller logic
