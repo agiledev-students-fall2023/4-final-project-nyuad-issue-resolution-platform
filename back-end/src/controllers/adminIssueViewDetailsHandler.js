46c2d271bb1a2ac7c694b825e4d32314723b78ea
Tauke190 <avinash@10-21-174-37.dynapool.wireless.nyu.edu>
2023-11-19 15:54:33 -0500

    admin get request it getting data from database now

--- back-end/src/controllers/adminIssueViewDetailsHandler.js
+++ back-end/src/controllers/adminIssueViewDetailsHandler.js
@@ -1,26 +1,14 @@
-import axios from "axios";
-import db from '../../database/db';
-
-// import IssueSchema from IssueSchema
+import { Issue } from '../../models/issues.mjs';
 
 // The function retrieves all the issues related to this department with index
 export async function adminIssueViewDetailsHandler(req, res) {
-  const { index } = req.params;
+  const { paramName } = req.params;
   // const { department } = req.params;
   try {
     // Assuming the data you want is at the response.data property
-    // const response = IssueSchema.findAll(index : index)
-    console.log(db);
-    const response = await axios.get(
-      `${process.env.BACKEND_URL}/json/mockapi.json` // will be replaced with db call
-    );
-
-    // Assuming response.data is an array of items and each item has a index
-    const filteredData = response.data.filter(
-      (item) => String(item.index) === String(index)
-    );
-
-    res.json(filteredData); // Send only the data that matches the specific issue index
+    const response = Issue.find({ index: paramName });
+    console.log(response);
+    res.json(response); // Send only the data that matches the specific issue index
   } catch (error) {
     // Log the error and send an appropriate response
     console.error("Error retrieving data:", error.message);
