87bab63a0d1cfc8fa02e3c4736606f548a33bb9f
hasiburratul <hasibur.ratul@gmail.com>
2023-11-24 12:21:49 -0500

    added node test env

--- back-end/src/middlewares/checkReferer.js
+++ back-end/src/middlewares/checkReferer.js
@@ -1,4 +1,8 @@
 export default function checkReferer(req, res, next) {
+    // Bypass referer check if in testing environment
+    if (process.env.NODE_ENV === 'test') {
+        return next();
+      }
     const referer = req.headers.referer;
     if (!referer || !referer.startsWith(process.env.FRONTEND_URL)) {
       return res.status(403).json({ message: "Access denied" });
