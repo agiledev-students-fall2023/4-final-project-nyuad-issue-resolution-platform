b99645db9eef7d423203c1399617f933820deb47
hasiburratul <hasibur.ratul@gmail.com>
2023-11-29 13:13:34 -0500

    updated login.test.js

--- back-end/test/login.test.js
+++ back-end/test/login.test.js
@@ -1,67 +1,5 @@
 /* eslint-disable */
-
 import chai, { assert } from "chai";
-import sinon from "sinon";
-import {
-  loginStudentHandler,
-  loginAdminHandler
-} from "../src/controllers/loginHandler.js";
-
-// Unit Testing
-describe("Unit Tests for Login Handlers", () => {
-  let req, res, statusSpy, jsonSpy;
-
-  beforeEach(() => {
-    req = { body: {} };
-    res = {
-      json: () => {},
-      status: function (s) {
-        this.statusCode = s;
-        return this;
-      }
-    };
-    statusSpy = sinon.spy(res, "status");
-    jsonSpy = sinon.spy(res, "json");
-  });
-
-  afterEach(() => {
-    statusSpy.restore();
-    jsonSpy.restore();
-  });
-
-  describe("loginStudentHandler", () => {
-    it("should authenticate valid student credentials", () => {
-      req.body = { username: "s", password: "1" };
-      loginStudentHandler(req, res);
-      assert(statusSpy.calledWith(200));
-      assert(jsonSpy.calledWith({ authenticated: true }));
-    });
-
-    it("should not authenticate invalid student credentials", () => {
-      req.body = { username: "wrong", password: "user" };
-      loginStudentHandler(req, res);
-      assert(statusSpy.calledWith(200));
-      assert(jsonSpy.calledWith({ authenticated: false }));
-    });
-  });
-
-  describe("loginAdminHandler", () => {
-    it("should authenticate valid admin credentials", () => {
-      req.body = { username: "a", password: "1" };
-      loginAdminHandler(req, res);
-      assert(jsonSpy.calledWith({ authenticated: true }));
-    });
-
-    it("should not authenticate invalid admin credentials", () => {
-      req.body = { username: "wrong", password: "user" };
-      loginAdminHandler(req, res);
-      assert(jsonSpy.calledWith({ authenticated: false }));
-    });
-  });
-});
-
-//Integration Testing
-
 import chaiHttp from "chai-http";
 import server from "../app.js";
 
@@ -73,9 +11,16 @@ describe("Integration Tests for Login Endpoints", () => {
       const res = await chai
         .request(server)
         .post("/api/login/student")
-        .send({ username: "s", password: "1" });
+        .send({ username: "student", password: "student" });
       assert.equal(res.status, 200);
-      assert.deepEqual(res.body, { authenticated: true });
+      assert.deepEqual(res.body, {
+        "__v": 0,
+        authenticated: true,
+        name: "student",
+        netId: "student",
+        userDept: "student",
+        userType: "student",
+      });
     });
 
     it("should not authenticate student with incorrect credentials", async () => {
@@ -84,7 +29,7 @@ describe("Integration Tests for Login Endpoints", () => {
         .post("/api/login/student")
         .send({ username: "wrong", password: "user" });
       assert.equal(res.status, 200);
-      assert.deepEqual(res.body, { authenticated: false });
+      assert.deepEqual(res.body, { authenticated: false});
     });
   });
 
@@ -93,9 +38,16 @@ describe("Integration Tests for Login Endpoints", () => {
       const res = await chai
         .request(server)
         .post("/api/login/admin")
-        .send({ username: "a", password: "1" });
+        .send({ username: "admin", password: "admin" });
       assert.equal(res.status, 200);
-      assert.deepEqual(res.body, { authenticated: true });
+      assert.deepEqual(res.body, {
+        "__v": 0,
+        authenticated: true,
+        name: "admin",
+        netId: "admin",
+        userDept: "admin",
+        userType: "admin",
+      });
     });
 
     it("should not authenticate admin with incorrect credentials", async () => {
