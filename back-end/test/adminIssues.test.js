f0aaddf1e6fb3b2ec3f4b556fe7c6dd3642bcd58
hasiburratul <hasibur.ratul@gmail.com>
2023-11-30 12:13:21 -0500

    added adminIssues test

---
+++ back-end/test/adminIssues.test.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import chai, { assert } from "chai";
+import IssueModel from "../models/issueModel.js";
+import chaiHttp from "chai-http";
+import server from "../app.js";
+
+chai.use(chaiHttp);
+
+process.env.NODE_ENV = "test";
+
+// Integration tests for the adminIssues.js file
+describe("Integration Tests for Admin Issue Handler Endpoint", () => {
+    describe("GET /api/issues/admin/:paramName", () => {
+        it("should retrieve all issues for a valid admin user", async () => {
+            const paramName = "admin";
+            const res = await chai
+                .request(server)
+                .get(`/api/issues/admin/${paramName}`);
+            // Check that the response is correct
+            assert.equal(res.status, 200);
+            // Check that the response is an array
+            assert.isArray(res.body);
+            // Check that the response is the same length as the number of issues
+            const userIssues = await IssueModel.find({ "departments": paramName });
+            // Check that the response is the same length as the number of issues of that user
+            assert.equal(res.body.length, userIssues.length);
+        });
+
+        it("should handle errors gracefully for an invalid admin user", async () => {
+            const paramName = "invalid";
+            const res = await chai
+                .request(server)
+                .get(`/api/issues/admin/${paramName}`);
+            assert.equal(res.status, 200);
+            assert.deepEqual(res.body, []);
+        });
+    });
+});
+/* eslint-enable */
