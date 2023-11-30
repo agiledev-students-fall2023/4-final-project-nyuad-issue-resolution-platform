2e394cfa8d9aab2190b5ba02386a81b108f5a3b4
hasiburratul <hasibur.ratul@gmail.com>
2023-11-30 14:23:41 -0500

    updated student issue display and testing

--- back-end/test/studentIssues.test.js
+++ back-end/test/studentIssues.test.js
@@ -35,6 +35,34 @@ describe("Integration Tests for Student Issue Handler Endpoint", () => {
       assert.equal(res.text, "User does not exist.");
     });
   });
+
+  describe("GET /api/issues/student/:studentNetID/:paramName", () => {
+    it("should retrieve all issues for a valid student NetID and issue index", async () => {
+      const studentNetID = "student";
+      const paramName = "101";
+      const res = await chai
+        .request(server)
+        .get(`/api/issues/student/${studentNetID}/${paramName}`);
+      // Check that the response is correct
+      assert.equal(res.status, 200);
+      // Check that the response is an array
+      assert.isArray(res.body);
+      // Check that the response is the same length as the number of issues
+      const userIssues = await IssueModel.find({ "studentNetID": studentNetID, "index": paramName });
+      // Check that the response is the same length as the number of issues of that user
+      assert.equal(res.body.length, userIssues.length);
+    });
+
+    it("should handle errors gracefully for an invalid issue index", async () => {
+      const studentNetID = "student";
+      const paramName = "01";
+      const res = await chai
+        .request(server)
+        .get(`/api/issues/student/${studentNetID}/${paramName}`);
+      assert.equal(res.status, 500); 
+      assert.equal(res.text, "No issues found for the given studentNetID and index.");
+    });
+  });
 });
 
 /* eslint-enable */
