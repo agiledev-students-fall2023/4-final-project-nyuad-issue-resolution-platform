5521924377639036a18f4f679ff7f64b88071ae6
hasiburratul <hasibur.ratul@gmail.com>
2023-12-03 12:31:42 -0500

    added admin post testing

--- back-end/src/controllers/adminPostHandler.js
+++ back-end/src/controllers/adminPostHandler.js
@@ -3,20 +3,14 @@ import Issue from '../../models/issueModel.js';
 export async function adminPostHandler(req, res) {
   const { paramName } = req.params;
   const { department } = req.params;
-  console.log(department);
-  // const issueindex = req.body.issueindex;
   const newcomment = req.body.commentbox;
   const currentStatus = req.body.issueStatus;
   const currentPriority = req.body.issuePriority;
   const departmentTags = req.body.issueDepartmentTags;
   const isProposed = req.body.isProposed;
   try {
-    const specificIssue = await Issue.findOne({ index: paramName });
-    if (!specificIssue) {
-      console.error('This specific issue could not found');
-      return;
-    }
-      if (newcomment !== undefined) {
+    const specificIssue = await Issue.findOne({ departments: department, index: paramName });
+    if (newcomment !== undefined) {
       specificIssue.comments.unshift(newcomment);
     }
     if (currentStatus !== undefined) {
@@ -42,9 +36,9 @@ export async function adminPostHandler(req, res) {
       specificIssue.isProposed = isProposed;
     }
     const updatedIssue = await specificIssue.save();
-    console.log('Issue updated:', updatedIssue);
-  } catch (err) {
-    console.error('Error updating issue:', err);
+    res.status(200).send("Success");
+  } catch (error) {
+    console.error('Error updating data:', error.message);
+    res.status(500).send("An error occurred while updating the data.");
   }
-  res.status(200).send("Success");
 }
